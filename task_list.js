45b8cbfe9366c4ca6c2486ef7f729644bfc9dcac
webdevandres <maciasandres02@gmail.com>
2020-07-24 19:38:06 -0500

    add set name for task list

--- task_list.js
+++ task_list.js
@@ -1,11 +1,13 @@
-var $ = function(id){
+var $ = function (id) {
     return document.getElementById(id);
 };
 var tasks = [];
+var sortDirection = "ASC";
 
-var displayTaskList = function(){
+
+var displayTaskList = function () {
     var list = "";
-    if(tasks.length === 0) {
+    if (tasks.length === 0) {
         // get tasks from storage or empty string if nothing is in storage
         var storage = localStorage.getItem("tasks") || "";
 
@@ -16,8 +18,13 @@ var displayTaskList = function(){
     }
     // if there are tasks in the array, sort and create a capitalized tasks string
     if (tasks.length > 0) {
-        var capitalized = tasks.sort().map(function(value){
-            var first = value.substring(0,1); // get first letter
+        if (sortDirection === "ASC") {
+            tasks.sort();
+        } else {
+            tasks.reverse();
+        }
+        var capitalized = tasks.map(function (value) {
+            var first = value.substring(0, 1); // get first letter
             var remaining = value.substring(1); //get remaining letters
             return first.toUpperCase() + remaining;
         });
@@ -26,8 +33,12 @@ var displayTaskList = function(){
     // display the tasks string and set focus on the task text box
     $("task_list").value = capitalized && capitalized.join("\n") || "";
     $("task").focus();
+
+    //display name if there is one in session storage
+    var setName = sessionStorage.firstName || "";
+    $("name").firstChild.nodeValue = setName.length === 0 ? "" : setName + "'s" + "List";
 };
-var addToTaskList = function() {
+var addToTaskList = function () {
     var task = $("task");
     if (task.value === "") {
         alert("Please enter a task.");
@@ -41,14 +52,38 @@ var addToTaskList = function() {
         displayTaskList();
     }
 };
-var clearTaskList = function(){
+var clearTaskList = function () {
     tasks.length = 0;
     localStorage.tasks = "";
     displayTaskList();
 };
 
-window.onload = function() {
+var deleteTask = function () {
+    var index = parseInt(prompt("Please enter the index of the task"));
+    if (!isNaN(index) || index === "") {
+        tasks.splice(index, 1);
+        localStorage.tasks = tasks.join("|");
+        displayTaskList();
+    }
+
+};
+
+var toggleSort = function () {
+    sortDirection = (sortDirection == "ASC") ? "DESC" : "ASC";
+    displayTaskList();
+};
+
+var setName = function () {
+    var name = prompt("Please enter a name");
+    sessionStorage.setItem("firstName", name);
+    displayTaskList();
+};
+
+window.onload = function () {
     $("add_task").onclick = addToTaskList;
     $("clear_tasks").onclick = clearTaskList;
+    $("delete_task").onclick = deleteTask;
+    $("toggle_sort").onclick = toggleSort;
+    $("set_name").onclick = setName;
     displayTaskList();
 };
